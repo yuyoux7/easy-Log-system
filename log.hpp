087d34f05090f3fa580ac4f01ef0e621c3bff176
yuyoux7 <yuyoux7@gmail.com>
2025-12-23 17:48:13 +0800

    Add files via upload

--- log.hpp
+++ log.hpp
@@ -2,253 +2,260 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <corecrt.h>
+#include <malloc.h>
+#include <string.h>
+#include <ctime>
 
-class Log {
-public:
-	enum info_list : int
-	{
-		unix_time	= 0x00001,
-		run_time	= 0x00010,
-		warning		= 0x00100,
-		error		= 0x01000,
-		static_string = 0x10000
-	};
-	Log() : filename(nullptr), st(time(NULL)) {
-		this->setLogheadinfo();
-	};
-	Log(std::string lg) : filename(nullptr), st(time(NULL))
-	{
-		this->Buff = lg + "\n";
-	};
-	friend std::ostream& operator<<(std::ostream& io, Log& lg)
-	{
-		lg.Buff += lg.flash;
-		io << lg.flash;
-		lg.flash.clear();
-		return io;
-	};
-	friend Log& operator<<(Log& lg, long long& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, long& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, int& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, short& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, unsigned int& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, unsigned long& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, std::string& str) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + str + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, char& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + in + '\n');
-		return lg;
-	};
-	friend Log& operator<<(Log& lg, const std::string& str) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + str + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const char* str) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + str + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const char& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + in + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const int& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const long& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const long long& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const unsigned int& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const unsigned long& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	}
-	friend Log& operator<<(Log& lg, const short& in) {
-		lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
-		lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
-		return lg;
-	}
-	friend std::string& operator<<(std::string& cl, int& val)
-	{
-		return cl += std::to_string(val);
-	};
-	Log& operator<<(Log& other) {
-		other.out = false;
-		other.Buff += other.flash;
-		other.flash.clear();
-		this->rw(other.Buff);
-		return *this;
-	};
-	template<typename T, typename ...C>
-	void LogWrite(T in, C... a);
-	void LogWrite() {
-		if (this->out) {
-			std::clog << *this;
-		}
-		else
+namespace Logger {
+	class Log {
+	public:
+		enum info_list : int const
 		{
-			this->Buff += this->flash;
-			this->flash.clear();
+			unix_time = 0x00001,
+			run_time = 0x00010,
+			warning = 0x00100,
+			error = 0x01000,
+			static_string = 0x10000
+		};
+		Log() : filename(nullptr), st(time(NULL)) {
+			this->setLogheadinfo();
+		};
+		template <typename T>
+		constexpr Log(T lg) : filename(nullptr), st(time(NULL))
+		{
+			this->infotype = Log::info_list::unix_time;
+			*this << lg;
+		};
+		friend std::ostream& operator<<(std::ostream& io, Log& lg)
+		{
+			lg.Buff += lg.flash;
+			io << lg.flash;
+			lg.flash.clear();
+			return io;
+		};
+		friend Log& operator<<(Log& lg, long long& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, long& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, int& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, short& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, unsigned int& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, unsigned long& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, std::string& str) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + str + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, char& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + in + '\n');
+			return lg;
+		};
+		friend Log& operator<<(Log& lg, const std::string& str) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + str + '\n');
+			return lg;
 		}
-		if ((this->filename != nullptr && tm != time(NULL)) || (olock && this->filename != nullptr)) {
-			std::ofstream ofile{};
-			ofile.open(this->filename);
-			if (ofile.is_open())
-			{
-				ofile << this->getbuff();
+		friend Log& operator<<(Log& lg, const char* str) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + str + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const char& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + in + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const int& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const long& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const long long& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const unsigned int& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const unsigned long& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		}
+		friend Log& operator<<(Log& lg, const short& in) {
+			lg.setLogheadinfo(lg.infotype, lg.logheadinfo);
+			lg.flash += (std::string("[" + lg.logheadinfo + "]:") + std::to_string(in) + '\n');
+			return lg;
+		}
+		friend std::string& operator<<(std::string& cl, int& val)
+		{
+			return cl += std::to_string(val);
+		};
+		Log& operator<<(Log& other) {
+			other.out = false;
+			other.Buff += other.flash;
+			other.flash.clear();
+			this->rw(other.Buff);
+			return *this;
+		};
+		template<typename T, typename ...C>
+		void LogWrite(T in, C... a);
+		void LogWrite() {
+			if (this->out) {
+				std::clog << *this;
 			}
-			ofile.close();
-			tm = time(NULL);
-		}
-	};
-	void SetLogName(const char* url = "log.txt");
-	void AutoOut(bool stat = true)
-	{
-		this->out = stat;
-	};
-	void setLogheadinfo(int basic_info_list = Log::unix_time, std::string str = std::string())
-	{
-		this->infotype = basic_info_list;
-		switch (basic_info_list)
+			else
+			{
+				this->Buff += this->flash;
+				this->flash.clear();
+			}
+			if ((this->filename != nullptr && tm != time(NULL)) || (olock && this->filename != nullptr)) {
+				std::ofstream ofile{};
+				ofile.open(this->filename);
+				if (ofile.is_open())
+				{
+					ofile << this->getbuff();
+				}
+				ofile.close();
+				tm = time(NULL);
+			}
+		};
+		void SetLogName(const char* url = "log.txt");
+		constexpr void AutoOut(bool stat = true)
 		{
-		case Log::unix_time:
-			this->logheadinfo = std::to_string(time(NULL));
-			break;
-		case Log::run_time:
-			this->logheadinfo = std::to_string((time(NULL) - this->st));
-			break;
-		case Log::warning:
-			this->logheadinfo = "warning";
-			break;
-		case Log::error:
-			this->logheadinfo = "error";
-			break;
-		case Log::static_string:
-			this->logheadinfo = str;
-			break;
-		case Log::unix_time | Log::warning:
-			this->logheadinfo = (std::to_string(time(NULL)) + " warning");
-			this->infotype = Log::static_string;
-			break;
-		case Log::unix_time | Log::error:
-			this->logheadinfo = (std::to_string(time(NULL)) + " error");
-			this->infotype = Log::static_string;
-			break;
-		case Log::unix_time | Log::static_string:
-			this->logheadinfo = (std::to_string(time(NULL)) + " " + str);
-			this->infotype = Log::static_string;
-			break;
-		case Log::run_time | Log::warning:
-			this->logheadinfo = (std::to_string((time(NULL) - this->st)) + " warning");
-			this->infotype = Log::static_string;
-			break;
-		case Log::run_time | Log::error:
-			this->logheadinfo = (std::to_string((time(NULL) - this->st)) + " error");
-			this->infotype = Log::static_string;
-			break;
-		case Log::run_time | Log::static_string:
-			this->logheadinfo = (std::to_string((time(NULL) - this->st)) + " " + str);
-			this->infotype = Log::static_string;
-			break;
-		default:
-			*this << "Not support this info type";
-			*this << "using basic type";
-			setLogheadinfo();
-			break;
-		}
+			this->out = stat;
+		};
+		constexpr void setLogheadinfo(int basic_info_list = Log::unix_time, std::string str = std::string())
+		{
+			this->infotype = basic_info_list;
+			switch (basic_info_list)
+			{
+			case Log::unix_time:
+				this->logheadinfo = std::to_string(time(NULL));
+				break;
+			case Log::run_time:
+				this->logheadinfo = std::to_string((time(NULL) - this->st));
+				break;
+			case Log::warning:
+				this->logheadinfo = "warning";
+				break;
+			case Log::error:
+				this->logheadinfo = "error";
+				break;
+			case Log::static_string:
+				this->logheadinfo = str;
+				break;
+			case Log::unix_time | Log::warning:
+				this->logheadinfo = (std::to_string(time(NULL)) + " warning");
+				this->infotype = Log::static_string;
+				break;
+			case Log::unix_time | Log::error:
+				this->logheadinfo = (std::to_string(time(NULL)) + " error");
+				this->infotype = Log::static_string;
+				break;
+			case Log::unix_time | Log::static_string:
+				this->logheadinfo = (std::to_string(time(NULL)) + " " + str);
+				this->infotype = Log::static_string;
+				break;
+			case Log::run_time | Log::warning:
+				this->logheadinfo = (std::to_string((time(NULL) - this->st)) + " warning");
+				this->infotype = Log::static_string;
+				break;
+			case Log::run_time | Log::error:
+				this->logheadinfo = (std::to_string((time(NULL) - this->st)) + " error");
+				this->infotype = Log::static_string;
+				break;
+			case Log::run_time | Log::static_string:
+				this->logheadinfo = (std::to_string((time(NULL) - this->st)) + " " + str);
+				this->infotype = Log::static_string;
+				break;
+			default:
+				setLogheadinfo();
+				*this << "Not support this info type";
+				*this << "using basic type";
+				break;
+			}
+		};
+		virtual ~Log() {
+			this->olock = true;
+			LogWrite();
+			if (this->filename != nullptr)
+			{
+				free(filename);
+				this->filename = nullptr;
+			}
+		};
+	private:
+		std::string Buff{};
+		std::string flash{};
+		char* filename{};
+		bool olock = false;
+		bool out = true;
+		int infotype{};
+		time_t tm{}, st{};
+		void rw(std::string lg) {
+			this->flash += lg;
+		};
+		const char* getbuff()
+		{
+			return Buff.c_str();
+		};
+		std::string logheadinfo{};
 	};
-	virtual ~Log() {
-		olock = true;
+
+	template<typename T, typename ...C>
+	inline void Log::LogWrite(T in, C... a)
+	{
+		*this << in;
+		if (sizeof...(a))
+		{
+			LogWrite(a...);
+		}
 		LogWrite();
-		if (filename != nullptr)
-		{
-			free(filename);
-			filename = nullptr;
+	};
+
+	inline void Log::SetLogName(const char* url)
+	{
+		this->filename = (char*)malloc(strlen(url) + 2);
+		if (this->filename != nullptr) {
+		#if defined(_WIN32)
+
+			strcpy_s(this->filename, (strlen(url) + 2), url);
+		#else
+			strcpy(this->filename, url);
+		#endif
 		}
 	};
-private:
-	std::string Buff{};
-	std::string flash{};
-	char* filename{};
-	bool olock = false;
-	bool out = true;
-	int infotype{};
-	time_t tm{}, st{};
-	void rw(std::string lg) {
-		this->flash += lg;
-	};
-	const char* getbuff()
-	{
-		return Buff.c_str();
-	};
-	std::string logheadinfo{};
-};
-
-template<typename T, typename ...C>
-inline void Log::LogWrite(T in, C... a)
-{
-	*this << in;
-	if (sizeof...(a))
-	{
-		LogWrite(a...);
-	}
-	LogWrite();
-
 }
-
-inline void Log::SetLogName(const char* url)
-{
-	this->filename = (char*)malloc(strlen(url) + 2);
-	if (this->filename != nullptr) {
-#if defined(_WIN32)
-
-		strcpy_s(this->filename, (strlen(url) + 2), url);
-#else
-		strcpy(this->filename, url);
-#endif
-	}
-};
